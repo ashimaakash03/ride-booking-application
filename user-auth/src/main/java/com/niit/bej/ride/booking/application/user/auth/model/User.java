b5191ea465a87284b24a492fb7fdd10c7cc5fccb
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 13:10:12 +0530

    added private fields in User.java model class

--- user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/model/User.java
+++ user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/model/User.java
@@ -1,4 +1,9 @@
 package com.niit.bej.ride.booking.application.user.auth.model;
 
 public class User {
+    private int id;
+    private String name;
+    private String contact;
+    private String email;
+    private String password;
 }
