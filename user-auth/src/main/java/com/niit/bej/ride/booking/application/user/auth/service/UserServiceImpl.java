2c86b929b727476cabe7b5bb5fa6d5bfb20aefba
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 14:20:11 +0530

    created UserServiceImpl.java class

---
+++ user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/service/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.niit.bej.ride.booking.application.user.auth.service;
+
+import com.niit.bej.ride.booking.application.user.auth.model.User;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public User registerAUser(User user) {
+        return null;
+    }
+
+    @Override
+    public User loginAUser(User user) {
+        return null;
+    }
+}
