54d2419b0486674dbf8c1ceb6ddb2a5be22d8cb3
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 14:11:32 +0530

    added constructor added @ResponseStatus annotation

--- user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/exception/UserNotFoundException.java
+++ user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/exception/UserNotFoundException.java
@@ -1,4 +1,11 @@
 package com.niit.bej.ride.booking.application.user.auth.exception;
 
-public class UserNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(reason = "User with given ID not found", code = HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
 }
