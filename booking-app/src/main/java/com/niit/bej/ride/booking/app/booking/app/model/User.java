73c8944a09813ee4c485233bd6bd225c646b124b
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 21:57:02 +0530

    added getters and setters in User.java class

--- booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/model/User.java
+++ booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/model/User.java
@@ -16,12 +16,51 @@ public class User {
 
     public User() {
     }
-
     public User(int id, String name, String contact, String email, List<Booking> bookings) {
         this.id = id;
         this.name = name;
         this.contact = contact;
         this.email = email;
+        this.bookings = bookings;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
         this.bookings = bookings;
     }
 }
