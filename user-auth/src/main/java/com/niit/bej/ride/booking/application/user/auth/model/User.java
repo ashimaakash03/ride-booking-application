012deefb3382c00753e04cbfb2e28c8a597f81cb
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 13:29:51 +0530

    added getters and setters in model class

--- user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/model/User.java
+++ user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/model/User.java
@@ -22,12 +22,51 @@ public class User {
 
     public User() {
     }
-
     public User(int id, String name, String contact, String email, String password) {
         this.id = id;
         this.name = name;
         this.contact = contact;
         this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
 }
