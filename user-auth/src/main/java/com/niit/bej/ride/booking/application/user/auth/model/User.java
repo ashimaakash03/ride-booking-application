45df63c559df632fe9aa2996ef472edba5e7907c
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 13:22:45 +0530

    added JPA annotations for table descriptions

--- user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/model/User.java
+++ user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/model/User.java
@@ -1,9 +1,22 @@
 package com.niit.bej.ride.booking.application.user.auth.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cab_users")
 public class User {
+    @Id
+    @Column(name = "id")
     private int id;
+    @Column(name = "user_name", length = 40, nullable = false)
     private String name;
+    @Column(name = "contact", length = 10, unique = true, nullable = false)
     private String contact;
+    @Column(name = "email", length = 50, unique = true, nullable = false)
     private String email;
+    @Column(name = "password", length = 25, unique = true, nullable = false)
     private String password;
 }
