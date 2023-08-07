6ba33273bffe7b97d1780efe8c299d5639126c10
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 14:58:02 +0530

    added @Autowired annotation

--- user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/controller/UserController.java
+++ user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/controller/UserController.java
@@ -2,6 +2,7 @@ package com.niit.bej.ride.booking.application.user.auth.controller;
 
 import com.niit.bej.ride.booking.application.user.auth.security.SecurityTokenGenerator;
 import com.niit.bej.ride.booking.application.user.auth.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ public class UserController {
     private final UserService userService;
     private final SecurityTokenGenerator securityTokenGenerator;
 
+    @Autowired
     public UserController(UserService userService, SecurityTokenGenerator securityTokenGenerator) {
         this.userService = userService;
         this.securityTokenGenerator = securityTokenGenerator;
