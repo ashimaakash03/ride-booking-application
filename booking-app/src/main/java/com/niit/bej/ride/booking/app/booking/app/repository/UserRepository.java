54bf53ea1683641b0292399816544e7292970f97
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 22:29:10 +0530

    added @Repository annotation

--- booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/repository/UserRepository.java
+++ booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.niit.bej.ride.booking.app.booking.app.repository;
 
-public interface UserRepository {
+import com.niit.bej.ride.booking.app.booking.app.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, Integer> {
 }
