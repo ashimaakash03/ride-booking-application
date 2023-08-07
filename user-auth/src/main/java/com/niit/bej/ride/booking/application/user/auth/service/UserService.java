00aecb1e50069446b425c220ce49c25e718aeed2
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 14:39:38 +0530

    implemented the register and login methods in UserServiceImpl.java class

--- user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/service/UserService.java
+++ user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/service/UserService.java
@@ -1,9 +1,12 @@
 package com.niit.bej.ride.booking.application.user.auth.service;
 
+import com.niit.bej.ride.booking.application.user.auth.exception.InvalidCredentialsException;
+import com.niit.bej.ride.booking.application.user.auth.exception.UserAlreadyExistsException;
+import com.niit.bej.ride.booking.application.user.auth.exception.UserNotFoundException;
 import com.niit.bej.ride.booking.application.user.auth.model.User;
 
 public interface UserService {
-    User registerAUser(User user);
+    User registerAUser(User user) throws UserAlreadyExistsException;
 
-    User loginAUser(User user);
+    boolean loginAUser(User user) throws InvalidCredentialsException, UserNotFoundException;
 }
--- user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/service/UserServiceImpl.java
+++ user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/service/UserServiceImpl.java
@@ -1,9 +1,14 @@
 package com.niit.bej.ride.booking.application.user.auth.service;
 
+import com.niit.bej.ride.booking.application.user.auth.exception.InvalidCredentialsException;
+import com.niit.bej.ride.booking.application.user.auth.exception.UserAlreadyExistsException;
+import com.niit.bej.ride.booking.application.user.auth.exception.UserNotFoundException;
 import com.niit.bej.ride.booking.application.user.auth.model.User;
 import com.niit.bej.ride.booking.application.user.auth.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -16,12 +21,27 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User registerAUser(User user) {
-        return null;
+    public User registerAUser(User user) throws UserAlreadyExistsException {
+        Optional<User> optionalUser = this.userRepository.findById(user.getId());
+        if (optionalUser.isPresent()) {
+            throw new UserAlreadyExistsException("User With ID: " + user.getId() + " already present");
+        } else {
+            return this.userRepository.save(user);
+        }
     }
 
     @Override
-    public User loginAUser(User user) {
-        return null;
+    public boolean loginAUser(User user) throws InvalidCredentialsException, UserNotFoundException {
+        Optional<User> optionalUser = this.userRepository.findById(user.getId());
+        if (optionalUser.isPresent()) {
+            User userInDatabase = optionalUser.get();
+            if (userInDatabase.getEmail().equals(user.getEmail()) && userInDatabase.getPassword().equals(user.getPassword())) {
+                return true;
+            } else {
+                throw new InvalidCredentialsException("Incorrect Email and Password");
+            }
+        } else {
+            throw new UserNotFoundException("User With ID: " + user.getId() + " is not present");
+        }
     }
 }
