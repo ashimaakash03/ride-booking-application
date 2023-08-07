919037fdd43e5cd0f2c52d6fe2a938a71f2924df
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 14:12:49 +0530

    modified UserNotFoundException.java

--- user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/exception/UserNotFoundException.java
+++ user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/exception/UserNotFoundException.java
@@ -3,7 +3,7 @@ package com.niit.bej.ride.booking.application.user.auth.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(reason = "User with given ID not found", code = HttpStatus.NOT_FOUND)
+@ResponseStatus(reason = "User With Given ID not found", code = HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends Exception {
     public UserNotFoundException(String message) {
         super(message);
