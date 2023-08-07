fc7663ed483ddd2e2ffdc0c3d926c1f20938619a
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 22:02:15 +0530

    added getters and setters in Booking.java class

--- booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/model/Booking.java
+++ booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/model/Booking.java
@@ -22,4 +22,52 @@ public class Booking {
         this.typeOfVehicle = typeOfVehicle;
         this.price = price;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPickupLocation() {
+        return pickupLocation;
+    }
+
+    public void setPickupLocation(String pickupLocation) {
+        this.pickupLocation = pickupLocation;
+    }
+
+    public String getDropLocation() {
+        return dropLocation;
+    }
+
+    public void setDropLocation(String dropLocation) {
+        this.dropLocation = dropLocation;
+    }
+
+    public float getDistanceInKM() {
+        return distanceInKM;
+    }
+
+    public void setDistanceInKM(float distanceInKM) {
+        this.distanceInKM = distanceInKM;
+    }
+
+    public String getTypeOfVehicle() {
+        return typeOfVehicle;
+    }
+
+    public void setTypeOfVehicle(String typeOfVehicle) {
+        this.typeOfVehicle = typeOfVehicle;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
