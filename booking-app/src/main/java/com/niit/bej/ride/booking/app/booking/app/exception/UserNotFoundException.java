9610fc65c568f2c3ed136d118f5a436b4c26c1ea
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 23:02:56 +0530

    added constructor to display message added @ResponseStatus annotation

--- booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/exception/UserNotFoundException.java
+++ booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/exception/UserNotFoundException.java
@@ -1,4 +1,11 @@
 package com.niit.bej.ride.booking.app.booking.app.exception;
 
-public class UserNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(reason = "User Couldn't be Found", code = HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
 }
