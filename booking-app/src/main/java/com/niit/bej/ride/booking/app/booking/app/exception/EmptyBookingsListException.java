2efec5f6690e65818a5eebd4696c17e4c32ed59a
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 23:19:01 +0530

    added constructor added @ResponseStatus annotation

--- booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/exception/EmptyBookingsListException.java
+++ booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/exception/EmptyBookingsListException.java
@@ -1,4 +1,11 @@
 package com.niit.bej.ride.booking.app.booking.app.exception;
 
-public class EmptyBookingsListException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(reason = "Bookings List Empty", code = HttpStatus.NO_CONTENT)
+public class EmptyBookingsListException extends Exception {
+    public EmptyBookingsListException(String message) {
+        super(message);
+    }
 }
