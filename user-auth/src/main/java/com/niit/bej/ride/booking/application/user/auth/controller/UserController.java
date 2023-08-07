bbe11f7bfaf342641437cd96913f4bed3d00805e
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 14:57:18 +0530

    added constructor dependency of user service and security token generator

--- user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/controller/UserController.java
+++ user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/controller/UserController.java
@@ -1,9 +1,18 @@
 package com.niit.bej.ride.booking.application.user.auth.controller;
 
+import com.niit.bej.ride.booking.application.user.auth.security.SecurityTokenGenerator;
+import com.niit.bej.ride.booking.application.user.auth.service.UserService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
+    private final UserService userService;
+    private final SecurityTokenGenerator securityTokenGenerator;
+
+    public UserController(UserService userService, SecurityTokenGenerator securityTokenGenerator) {
+        this.userService = userService;
+        this.securityTokenGenerator = securityTokenGenerator;
+    }
 }
