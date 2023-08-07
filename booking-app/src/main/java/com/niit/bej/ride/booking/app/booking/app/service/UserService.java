a6b2044d661873903267c1488d07e7da931de19b
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 22:53:54 +0530

    added abstract methods in UserService.java interface

--- booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/service/UserService.java
+++ booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/service/UserService.java
@@ -1,4 +1,26 @@
 package com.niit.bej.ride.booking.app.booking.app.service;
 
+import com.niit.bej.ride.booking.app.booking.app.model.Booking;
+import com.niit.bej.ride.booking.app.booking.app.model.User;
+
+import java.util.List;
+
 public interface UserService {
+    User createNewUser(User user);
+
+    User fetchUserByName(String name);
+
+    User fetchUserByEmail(String email);
+
+    Booking addNewBooking(Booking booking);
+
+    List<Booking> viewAllBookingsOfUser(int userId);
+
+    Booking findSpecificBooking(int userId, int bookingId);
+
+    Booking updateABooking(int userId, int bookingId, Booking booking);
+
+    boolean deleteABookingOfUser(int userId, int bookingId);
+
+    boolean deleteAllBookingsOfUser(int userId);
 }
