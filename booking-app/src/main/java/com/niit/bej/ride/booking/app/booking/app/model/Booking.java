a880865def45b4b78ab599e5e858b8e619a32626
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 21:45:23 +0530

    added private fields in Booking.java class

--- booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/model/Booking.java
+++ booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/model/Booking.java
@@ -1,4 +1,10 @@
 package com.niit.bej.ride.booking.app.booking.app.model;
 
 public class Booking {
+    private int id;
+    private String pickupLocation;
+    private String dropLocation;
+    private float distanceInKM;
+    private String typeOfVehicle;
+    private float price;
 }
