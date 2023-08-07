52b6c244faa160fc3a16e31e7e6852b67d0bc71e
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 14:51:54 +0530

    added implementation of generateToken() method

--- user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/security/JwtSecurityTokenGenerator.java
+++ user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/security/JwtSecurityTokenGenerator.java
@@ -1,14 +1,19 @@
 package com.niit.bej.ride.booking.application.user.auth.security;
 
 import com.niit.bej.ride.booking.application.user.auth.model.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.Map;
 
 @Service
 public class JwtSecurityTokenGenerator implements SecurityTokenGenerator {
     @Override
     public Map<String, String> generateToken(User user) {
-        return null;
+        String jwtToken = Jwts.builder().setIssuedAt(new Date()).setSubject(user.getName()).signWith(SignatureAlgorithm.HS256, "password").compact();
+        return Map.of("token", jwtToken,
+                "message", user.getName() + " logged in!");
     }
 }
