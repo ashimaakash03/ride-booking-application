d135f79ff202917daa4f0e94e9e4d5ab4bd8ed6c
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 23:16:02 +0530

    added constructor added @ResponseStatus annotation

--- booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/exception/BookingNotFoundException.java
+++ booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/exception/BookingNotFoundException.java
@@ -1,4 +1,11 @@
 package com.niit.bej.ride.booking.app.booking.app.exception;
 
-public class BookingNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(reason = "Booking Couldn't be Found", code = HttpStatus.NOT_FOUND)
+public class BookingNotFoundException extends Exception {
+    public BookingNotFoundException(String message) {
+        super(message);
+    }
 }
