32770138ed852295eeabd7f6bea8af16559fdba2
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 14:15:47 +0530

    added constructor added @ResponseStatus annotation

--- user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/exception/InvalidCredentialsException.java
+++ user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/exception/InvalidCredentialsException.java
@@ -1,4 +1,11 @@
 package com.niit.bej.ride.booking.application.user.auth.exception;
 
-public class InvalidCredentialsException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(reason = "Email And Password Doesn't match", code = HttpStatus.CONFLICT)
+public class InvalidCredentialsException extends Exception {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
 }
