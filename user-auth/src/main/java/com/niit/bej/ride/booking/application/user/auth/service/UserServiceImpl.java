62257d9f51e92bfee63e957626f7071525e71625
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 14:22:47 +0530

    added constructor dependency to UserRepository interface

--- user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/service/UserServiceImpl.java
+++ user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/service/UserServiceImpl.java
@@ -1,10 +1,20 @@
 package com.niit.bej.ride.booking.application.user.auth.service;
 
 import com.niit.bej.ride.booking.application.user.auth.model.User;
+import com.niit.bej.ride.booking.application.user.auth.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public User registerAUser(User user) {
         return null;
