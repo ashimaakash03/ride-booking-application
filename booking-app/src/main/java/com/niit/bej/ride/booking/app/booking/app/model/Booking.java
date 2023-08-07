fc1d7be4afdfa2dd72ef0b1d04f318b870bae705
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 21:49:05 +0530

    added @MongoId annotation in Booking.java class

--- booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/model/Booking.java
+++ booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/model/Booking.java
@@ -1,6 +1,9 @@
 package com.niit.bej.ride.booking.app.booking.app.model;
 
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
 public class Booking {
+    @MongoId
     private int id;
     private String pickupLocation;
     private String dropLocation;
