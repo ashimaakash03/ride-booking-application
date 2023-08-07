7530481e669fd623a4ac22b9077b01b9ccee1149
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 14:44:54 +0530

    added @Service annotation

--- user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/security/JwtSecurityTokenGenerator.java
+++ user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/security/JwtSecurityTokenGenerator.java
@@ -1,9 +1,11 @@
 package com.niit.bej.ride.booking.application.user.auth.security;
 
 import com.niit.bej.ride.booking.application.user.auth.model.User;
+import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
+@Service
 public class JwtSecurityTokenGenerator implements SecurityTokenGenerator {
     @Override
     public Map<String, String> generateToken(User user) {
