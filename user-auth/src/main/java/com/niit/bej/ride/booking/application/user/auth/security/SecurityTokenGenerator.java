c5ec808349c09193067d91150c922c1b16f67209
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 14:42:53 +0530

    added generateToken() method

--- user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/security/SecurityTokenGenerator.java
+++ user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/security/SecurityTokenGenerator.java
@@ -1,4 +1,9 @@
 package com.niit.bej.ride.booking.application.user.auth.security;
 
+import com.niit.bej.ride.booking.application.user.auth.model.User;
+
+import java.util.Map;
+
 public interface SecurityTokenGenerator {
+    Map<String, String> generateToken(User user);
 }
