293a3e934eda805a55245f97e7bab384a01874c0
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 14:21:18 +0530

    added @Service annotation on UserServiceImpl.java class

--- user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/service/UserServiceImpl.java
+++ user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/service/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package com.niit.bej.ride.booking.application.user.auth.service;
 
 import com.niit.bej.ride.booking.application.user.auth.model.User;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
     @Override
     public User registerAUser(User user) {
