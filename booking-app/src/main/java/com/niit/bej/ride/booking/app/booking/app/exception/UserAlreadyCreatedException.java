bf11b9f7d7cc16a6f57e58df8f2f62e069f6477f
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 22:58:34 +0530

    added @ResponseStatus annotation

--- booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/exception/UserAlreadyCreatedException.java
+++ booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/exception/UserAlreadyCreatedException.java
@@ -1,4 +1,11 @@
 package com.niit.bej.ride.booking.app.booking.app.exception;
 
-public class UserAlreadyCreatedException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(reason = "User Already Created", code = HttpStatus.CONFLICT)
+public class UserAlreadyCreatedException extends Exception {
+    public UserAlreadyCreatedException(String message) {
+        super(message);
+    }
 }
