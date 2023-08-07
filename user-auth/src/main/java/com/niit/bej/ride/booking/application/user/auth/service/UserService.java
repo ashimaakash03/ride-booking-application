7acd13f0d4ea4c764cdad2ca9ba39c3add0cb973
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 13:52:58 +0530

    added abstract methods to register and login a user

--- user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/service/UserService.java
+++ user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/service/UserService.java
@@ -1,4 +1,9 @@
 package com.niit.bej.ride.booking.application.user.auth.service;
 
+import com.niit.bej.ride.booking.application.user.auth.model.User;
+
 public interface UserService {
+    User registerAUser(User user);
+
+    User loginAUser(User user);
 }
