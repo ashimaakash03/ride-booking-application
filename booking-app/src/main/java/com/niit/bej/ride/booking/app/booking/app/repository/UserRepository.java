2a30f3748632031e224537855bb8377b1b08c813
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 22:34:06 +0530

    added custom methods in UserRepository.java interface

--- booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/repository/UserRepository.java
+++ booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/repository/UserRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, Integer> {
+    User findUserByName(String name);
+
+    User findUserByEmail(String email);
 }
