090e1d4849767fbe493f2573c73d7e8f9208b69e
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 14:18:34 +0530

    added constructor added @ResponseStatus annotation

--- user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/exception/EmptyUsersDatabaseException.java
+++ user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/exception/EmptyUsersDatabaseException.java
@@ -1,4 +1,11 @@
 package com.niit.bej.ride.booking.application.user.auth.exception;
 
-public class EmptyUsersDatabaseException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(reason = "No Users Present in Database", code = HttpStatus.NO_CONTENT)
+public class EmptyUsersDatabaseException extends Exception {
+    public EmptyUsersDatabaseException(String message) {
+        super(message);
+    }
 }
