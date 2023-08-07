a4e2542d494f83eec3cb5ced5b5dcb03317dbebb
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 14:08:01 +0530

    added constructor added @ResponseStatus annotation

--- user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/exception/UserAlreadyExistsException.java
+++ user-auth/src/main/java/com/niit/bej/ride/booking/application/user/auth/exception/UserAlreadyExistsException.java
@@ -1,4 +1,11 @@
 package com.niit.bej.ride.booking.application.user.auth.exception;
 
-public class UserAlreadyExistsException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(reason = "User With Given ID already present", code = HttpStatus.CONFLICT)
+public class UserAlreadyExistsException extends Exception {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
 }
