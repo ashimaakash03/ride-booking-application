2b56ce5c9bd97ee41ffab072d365ea8fb1704c5b
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 21:38:13 +0530

    added Mongo annotations in User.java class

--- booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/model/User.java
+++ booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/model/User.java
@@ -1,8 +1,15 @@
 package com.niit.bej.ride.booking.app.booking.app.model;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.util.List;
+
+@Document(collection = "bookings")
 public class User {
+    @MongoId
     private int id;
     private String name;
     private String email;
-    //private List<Booking> bookings;
+    private List<Booking> bookings;
 }
