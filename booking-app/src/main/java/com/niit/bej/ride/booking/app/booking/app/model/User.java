93c322f2d53b6f343d3756b14d2d8fe4353ae5a8
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-07 21:35:12 +0530

    added private variables in User.java class

--- booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/model/User.java
+++ booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/model/User.java
@@ -1,4 +1,8 @@
 package com.niit.bej.ride.booking.app.booking.app.model;
 
 public class User {
+    private int id;
+    private String name;
+    private String email;
+    //private List<Booking> bookings;
 }
