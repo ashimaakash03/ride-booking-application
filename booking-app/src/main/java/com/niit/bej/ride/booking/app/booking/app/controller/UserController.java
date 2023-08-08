872b2267c8448e9f5c2f3d433b666e7a1e74ae66
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-08 10:49:32 +0530

    added user service as a dependency added @Autowired annotation

--- booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/controller/UserController.java
+++ booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/controller/UserController.java
@@ -1,10 +1,17 @@
 package com.niit.bej.ride.booking.app.booking.app.controller;
 
+import com.niit.bej.ride.booking.app.booking.app.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
+    private final UserService userService;
 
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 }
