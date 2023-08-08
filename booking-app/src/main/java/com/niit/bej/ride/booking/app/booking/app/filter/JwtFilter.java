e5d5db34ac9ed7ee9f913ab3a89b1ed7b28d57b0
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-08 12:56:59 +0530

    added 'rides' endpoint in request mapping added @WebFilter annotation and url patterns

--- booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/filter/JwtFilter.java
+++ booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/filter/JwtFilter.java
@@ -1,4 +1,8 @@
 package com.niit.bej.ride.booking.app.booking.app.filter;
 
-public class JwtFilter {
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+
+@WebFilter(urlPatterns = "/api/v1/rides/**")
+public class JwtFilter extends HttpFilter {
 }
