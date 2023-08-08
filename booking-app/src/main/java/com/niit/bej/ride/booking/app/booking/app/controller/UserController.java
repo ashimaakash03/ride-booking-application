ba16aff2c064e5e2f3135877cab6c8d263cdccc1
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-08 10:11:20 +0530

    added @RestController and @RequestMapping annotations

--- booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/controller/UserController.java
+++ booking-app/src/main/java/com/niit/bej/ride/booking/app/booking/app/controller/UserController.java
@@ -1,4 +1,9 @@
 package com.niit.bej.ride.booking.app.booking.app.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
 public class UserController {
 }
