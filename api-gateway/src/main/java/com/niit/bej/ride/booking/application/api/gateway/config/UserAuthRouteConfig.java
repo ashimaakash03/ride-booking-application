634914bcbf160110a8d4c1d6df3549324b66a4b7
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-09 13:30:49 +0530

    added @Configuration and @LoadBalancerClient annotation

--- api-gateway/src/main/java/com/niit/bej/ride/booking/application/api/gateway/config/UserAuthRouteConfig.java
+++ api-gateway/src/main/java/com/niit/bej/ride/booking/application/api/gateway/config/UserAuthRouteConfig.java
@@ -1,4 +1,10 @@
 package com.niit.bej.ride.booking.application.api.gateway.config;
 
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@LoadBalancerClient(name = "user-auth-service")
 public class UserAuthRouteConfig {
+
 }
