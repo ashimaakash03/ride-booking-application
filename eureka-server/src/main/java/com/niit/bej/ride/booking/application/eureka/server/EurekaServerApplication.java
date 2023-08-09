f9cfa1778df1b7f5e49f82aaf873ae317c9f18a2
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-09 21:35:02 +0530

    added @EnableEurekaServer annotation

--- eureka-server/src/main/java/com/niit/bej/ride/booking/application/eureka/server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/niit/bej/ride/booking/application/eureka/server/EurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.niit.bej.ride.booking.application.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
