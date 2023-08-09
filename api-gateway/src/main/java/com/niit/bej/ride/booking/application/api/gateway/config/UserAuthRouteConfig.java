3a4a56f9c416a48aa92adb4b43ba54af62a5dbb7
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-09 13:38:37 +0530

    completed UserAuthRouteConfig.java class

--- api-gateway/src/main/java/com/niit/bej/ride/booking/application/api/gateway/config/UserAuthRouteConfig.java
+++ api-gateway/src/main/java/com/niit/bej/ride/booking/application/api/gateway/config/UserAuthRouteConfig.java
@@ -1,10 +1,23 @@
 package com.niit.bej.ride.booking.application.api.gateway.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @LoadBalancerClient(name = "user-auth-service")
 public class UserAuthRouteConfig {
 
+    @Bean
+    @LoadBalanced
+    public RouteLocator locateUserAuthMicroserviceRoutes(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder
+                .routes()
+                .route(routePredicate -> routePredicate.path("/api/v1/**")
+                        .uri("lb://user-auth-service"))
+                .build();
+    }
 }
