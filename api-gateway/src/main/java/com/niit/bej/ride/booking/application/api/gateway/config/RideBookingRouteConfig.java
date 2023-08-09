86015d3426a75c30106e0c634a711a218dde3561
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-09 13:48:37 +0530

    completed RideBookingRouteConfig.java class

--- api-gateway/src/main/java/com/niit/bej/ride/booking/application/api/gateway/config/RideBookingRouteConfig.java
+++ api-gateway/src/main/java/com/niit/bej/ride/booking/application/api/gateway/config/RideBookingRouteConfig.java
@@ -1,9 +1,23 @@
 package com.niit.bej.ride.booking.application.api.gateway.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @LoadBalancerClient(name = "booking-app-service")
 public class RideBookingRouteConfig {
+
+    @Bean
+    @LoadBalanced
+    public RouteLocator locateBookingAppMicroserviceRoutes(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder
+                .routes()
+                .route(routePredicate -> routePredicate.path("/api/v1/rides/**")
+                        .uri("lb://booking-app-service"))
+                .build();
+    }
 }
