decedf7a7da8c8c5676a6368f210f5716db73aab
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-13 00:08:30 +0530

    generated registration component

--- ride-booking-application-frontend/src/app/app.module.ts
+++ ride-booking-application-frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HeaderComponent} from './components/header/header.component';
+import {RegistrationComponent} from './components/registration/registration.component';
 
 @NgModule({
   declarations: [
 	  AppComponent,
 	  HeaderComponent,
+	  RegistrationComponent,
   ],
   imports: [
     BrowserModule,
