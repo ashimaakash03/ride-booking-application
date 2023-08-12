71d257055657c3472041fced4ef8fce433365321
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-12 23:44:10 +0530

    removed material header component

--- ride-booking-application-frontend/src/app/app.module.ts
+++ ride-booking-application-frontend/src/app/app.module.ts
@@ -4,28 +4,15 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {HeaderComponent} from './components/header/header.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list';
-
 
 @NgModule({
   declarations: [
 	  AppComponent,
-	  HeaderComponent,
   ],
   imports: [
     BrowserModule,
 	  AppRoutingModule,
 	  BrowserAnimationsModule,
-	  MatToolbarModule,
-	  MatButtonModule,
-	  MatSidenavModule,
-	  MatIconModule,
-	  MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
