ef1d2a1e24871214dec0e605dc169a9ca02ae646
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-14 00:06:01 +0530

    added fields in user.ts class added constructor

--- ride-booking-application-frontend/src/app/models/user.ts
+++ ride-booking-application-frontend/src/app/models/user.ts
@@ -1,2 +1,15 @@
 export class User {
+	id: number;
+	fullname: string;
+	contact: string;
+	email: string;
+	password: string;
+
+	constructor(id: number, fullname: string, contact: string, email: string, password: string) {
+		this.id = id;
+		this.fullname = fullname;
+		this.contact = contact;
+		this.email = email;
+		this.password = password;
+	}
 }
