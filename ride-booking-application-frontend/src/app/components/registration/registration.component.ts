ae8c8fe4594be26bcc39b7774ce035a79558b050
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-13 13:22:27 +0530

    completed registration form

--- ride-booking-application-frontend/src/app/components/registration/registration.component.ts
+++ ride-booking-application-frontend/src/app/components/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
 	selector: 'app-registration',
@@ -11,11 +11,23 @@ export class RegistrationComponent implements OnInit {
 	registrationForm: FormGroup;
 
 	constructor(private formBuilder: FormBuilder) {
-		this.registrationForm = this.formBuilder.group({});
+		this.registrationForm = this.formBuilder.group({
+			fullname: ['', [Validators.required]],
+			contact: ['', [Validators.required, Validators.pattern]],
+			email: ['', [Validators.required, Validators.pattern]],
+			password: ['', [Validators.required]]
+		});
 	}
 
 	ngOnInit(): void {
-
+		let submitForm = document.querySelector('form') as HTMLFormElement;
+		submitForm.addEventListener('submit', (submitEvent: SubmitEvent) => {
+			if (!submitForm.checkValidity()) {
+				submitEvent.preventDefault();
+				submitEvent.stopPropagation();
+			}
+			submitForm.classList.add('was-validated');
+		});
 	}
 
 }
