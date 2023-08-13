ceb346bd1e77d787e89dad1df809f0b795e05da7
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-13 18:41:39 +0530

    added booking form validations in bookings.component.ts

--- ride-booking-application-frontend/src/app/components/bookings/bookings.component.ts
+++ ride-booking-application-frontend/src/app/components/bookings/bookings.component.ts
@@ -1,11 +1,36 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
 	selector: 'app-bookings',
 	templateUrl: './bookings.component.html',
 	styleUrls: ['./bookings.component.css']
 })
-export class BookingsComponent {
-	bookingForm: any;
+export class BookingsComponent implements OnInit {
+
+	bookingForm: FormGroup;
+
+	constructor(private formBuilder: FormBuilder) {
+		this.bookingForm = this.formBuilder.group({
+			fullname: ['', [Validators.required]],
+			contact: ['', [Validators.required, Validators.pattern]],
+			fromlocation: ['', [Validators.required]],
+			tolocation: ['', [Validators.required]],
+			vehicle: ['', [Validators.required]]
+		});
+	}
+
+	ngOnInit(): void {
+		let submitForm = document.querySelector('form') as HTMLFormElement
+
+		submitForm.addEventListener('submit', (submitEvent: SubmitEvent) => {
+			if (!submitForm.checkValidity()) {
+				submitEvent.stopPropagation();
+				submitEvent.preventDefault();
+			}
+			submitForm.classList.add('was-validated');
+			submitForm.reset();
+		});
+	}
 
 }
