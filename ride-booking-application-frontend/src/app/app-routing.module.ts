f7b0bccb645da20ea7974439a0ad28b1659e6a58
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-13 15:54:13 +0530

    added route to booking component

--- ride-booking-application-frontend/src/app/app-routing.module.ts
+++ ride-booking-application-frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {RegistrationComponent} from "./components/registration/registration.component";
 import {LoginComponent} from "./components/login/login.component";
+import {BookingsComponent} from "./components/bookings/bookings.component";
 
 const routes: Routes = [
 	{path: '', component: RegistrationComponent, pathMatch: 'full'},
 	{path: 'sign-up', component: RegistrationComponent},
-	{path: 'sign-in', component: LoginComponent}
+	{path: 'sign-in', component: LoginComponent},
+	{path: 'booking', component: BookingsComponent}
 ];
 
 @NgModule({
