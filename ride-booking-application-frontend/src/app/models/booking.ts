7b38ea179ecff29829878bef93daa159ad33d5a3
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-14 00:14:16 +0530

    added fields in booking.ts class added constructor

--- ride-booking-application-frontend/src/app/models/booking.ts
+++ ride-booking-application-frontend/src/app/models/booking.ts
@@ -1,2 +1,18 @@
 export class Booking {
+	id: number;
+	pickuplocation: string;
+	droplocation: string;
+	vehicletype: string;
+	distancetravelled: number;
+	costofride: number;
+
+	constructor(id: number, pickuplocation: string, droplocation: string,
+				vehicletype: string, distancetravelled: number, costofride: number) {
+		this.id = id;
+		this.pickuplocation = pickuplocation;
+		this.droplocation = droplocation;
+		this.vehicletype = vehicletype;
+		this.distancetravelled = distancetravelled;
+		this.costofride = costofride;
+	}
 }
