37edaef2a48993eb2dc24783e43384328793f0eb
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-13 09:58:01 +0530

    added routing in Routes[] of app-routing.module.ts

--- ride-booking-application-frontend/src/app/app-routing.module.ts
+++ ride-booking-application-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {RegistrationComponent} from "./components/registration/registration.component";
+import {LoginComponent} from "./components/login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+	{path: '', component: RegistrationComponent, pathMatch: 'full'},
+	{path: 'sign-up', component: RegistrationComponent},
+	{path: 'sign-in', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
