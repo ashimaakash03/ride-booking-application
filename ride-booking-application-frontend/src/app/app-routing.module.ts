ebe8da352ac70d967d8ede8eb51e903e8d2420af
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-13 22:57:15 +0530

    modified booking-history template

--- ride-booking-application-frontend/src/app/app-routing.module.ts
+++ ride-booking-application-frontend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import {RouterModule, Routes} from '@angular/router';
 import {RegistrationComponent} from "./components/registration/registration.component";
 import {LoginComponent} from "./components/login/login.component";
 import {BookingsComponent} from "./components/bookings/bookings.component";
+import {BookingHistoryComponent} from "./components/booking-history/booking-history.component";
 
 const routes: Routes = [
 	{path: '', component: RegistrationComponent, pathMatch: 'full'},
-	{path: 'sign-up', component: RegistrationComponent},
-	{path: 'sign-in', component: LoginComponent},
-	{path: 'booking', component: BookingsComponent}
+	{path: 'sign-up', component: RegistrationComponent, pathMatch: 'full'},
+	{path: 'sign-in', component: LoginComponent, pathMatch: 'full'},
+	{path: 'booking', component: BookingsComponent, pathMatch: 'full'},
+	{path: 'history', component: BookingHistoryComponent, pathMatch: 'full'}
 ];
 
 @NgModule({
