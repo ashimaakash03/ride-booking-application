06d54b404a7d4db701ee0f78f8262bd54b6891d2
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-13 11:27:46 +0530

    added formControlName to all form fields

--- ride-booking-application-frontend/src/app/components/registration/registration.component.ts
+++ ride-booking-application-frontend/src/app/components/registration/registration.component.ts
@@ -1,11 +1,21 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
 	selector: 'app-registration',
 	templateUrl: './registration.component.html',
 	styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent {
-	registrationForm: any;
+export class RegistrationComponent implements OnInit {
+
+	registrationForm: FormGroup;
+
+	constructor(private formBuilder: FormBuilder) {
+		this.registrationForm = this.formBuilder.group({});
+	}
+
+	ngOnInit(): void {
+
+	}
 
 }
