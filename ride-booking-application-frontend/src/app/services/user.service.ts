f4f4723e7970d03cb4dfa2ef5e8311bf6d2c6352
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-15 23:04:13 +0530

    created addUser() method in user.service.ts

--- ride-booking-application-frontend/src/app/services/user.service.ts
+++ ride-booking-application-frontend/src/app/services/user.service.ts
@@ -1,10 +1,18 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {User} from "../models/user";
 
 @Injectable({
 	providedIn: 'root'
 })
 export class UserService {
+	url: string = "http://localhost:3000/registered-users";
 
-	constructor() {
+	constructor(private httpClient: HttpClient) {
+	}
+
+	addUser(user: User): Observable<Array<User>> {
+		return this.httpClient.post<Array<User>>(this.url, user);
 	}
 }
